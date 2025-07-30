b07323960a72e53f68e411398040e7d60e5bf200
Muhammad Zeeshan Ali <140070369+zeeshanali66@users.noreply.github.com>
2025-07-30 18:39:50 +0500

    Update app.py

--- app.py
+++ app.py
@@ -54,5 +54,5 @@ def detect_emotion_route():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 6000))
     app.run(host='0.0.0.0', port=port)
